0819420bd19a20b0fe311ad5003693a1a76d0dda
ashok.atce <ashok.atce@gmail.com>
2022-10-20 10:24:50 +0530

    #1 first improvement

--- Js/index.js
+++ Js/index.js
@@ -1,9 +1,14 @@
 // document.ready(function(){
  
-   function preLoader(){
-    var preloader=$('#preloader');
-    preloader.css('display','none');
+//    function preLoader(){
+//     var preloader=$('#preloader');
+//     preloader.css('display','none');
     
-    }
+//     }
    
 // })
+
+
+$(window).on('load', function () {
+    $("#preloader").css('display','none');
+    });
