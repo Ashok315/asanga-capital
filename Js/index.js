ac6f5fde89d63047b9248781e675d487cd4f3a6d
ashok.atce <ashok.atce@gmail.com>
2023-04-20 19:58:19 +0530

    optimize all pages

--- Js/index.js
+++ Js/index.js
@@ -1,10 +1,10 @@
 
-
+$(document).ready(function(){
+  $('#nav-bar').load('navbar.html');
+  $('#footer').load('footer.html');
+})
 
 $(window).on('load', function () {
     $("#preloader").css('display','none');
     });
 
-//  $('.carousel').carousel({
-//         interval: 1000
-//       });
